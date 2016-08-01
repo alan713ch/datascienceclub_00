1b39267924c936082bc6d1e93be2c3b9aa1adf15
Alan <izar.alan@gmail.com>
2016-08-01 16:43:08 -0600

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,15 @@
+#import pandas and numpy
+
+import pandas as pd
+import numpy as np
+import sys
+
+#display versions of each package
+
+print('\npython version ' + sys.version)
+print('pandas version ' + pd.__version__)
+print('numpy version ' + np.__version__)
+
+#say hello!
+print('\nHello world!')
+
